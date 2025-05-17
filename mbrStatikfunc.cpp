05f72b8d06fdcc53e1896505d3359849102dc9e0
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-05-17 15:19:52 +0700

    menamahkan class mahasiswa

---
+++ mbrStatikfunc.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa{
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printALL();
+
+    static void setNim(int pNim){nim=pNim; /*Definisi Function*/}
+    static int getNim(){return nim; /*Definisi Function*/}
+
+    mahasiswa(string pnama):nama(pnama){setID();}
+};
+
+int mahasiswa::nim=0;
