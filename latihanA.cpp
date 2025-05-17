b09f680ce6c98e3a793eba4858909d23d224e6a9
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-05-17 16:20:41 +0700

    menambahkan class dosen

--- latihanA.cpp
+++ latihanA.cpp
@@ -2,10 +2,13 @@
 #include <string>
 using namespace std;
 
+class dosen;
+class universitas;
+
 class mahasiswa{
 private :
     string nama;
-    string nim;
+    int nim;
     float nilai;
 public:
     mahasiswa(string nama, string nim) : nama(nama), nim(nim), nilai(0){}
@@ -18,5 +21,24 @@ public:
     friend class dosen;
 };
 
+class dosen{
+private : 
+    string nama;
+    int NIDN;
+    string pangkat;
+    float gaji;
+public :
+    dosen(string nama, int NIDN, string pangkat, float gaji) : nama(nama), NIDN(NIDN), pangkat(pangkat), gaji(gaji){}
 
+    void beriNilai(mahasiswa* M, float nilai) {
+        M->nilai = nilai;
+    }
+    void tampilkanInfo() {
+        cout<<"Nama Dosen : "<<nama<<endl;
+        cout<<"NIDN : "<<NIDN<<endl;
+        cout<<"Pangkat :"<<pangkat<<endl;
+    }
+    friend void LihatGajiDosen(dosen* d);
+    friend class staf;
+}
 
