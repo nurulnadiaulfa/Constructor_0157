e4b8b3af2e8a955084a5f6f3cbae3d1e8a1ab75f
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-05-17 15:11:21 +0700

    menambahkan class mahasiswa

---
+++ konstruktor.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa{
+private:
+    int nim;
+    string nama;
+public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
+mahasiswa :: mahasiswa(){
+    nim = 0;
+    nama = "";
+}
+
+mahasiswa :: mahasiswa(int iNim){
+    nim = iNim;
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama;
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim;
+    nama = iNama;
+}
